3858350c7669d352b19bfdeee048190d9e93ca78
michaelDORY <mish.makwww@gmail.com>
2025-04-11 19:35:03 +0300

    feat: add lottie animation

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,9 +1,19 @@
 'use client';
+import { useLottie } from 'lottie-react';
+import loadingAnimation from '../public/valmira-lottie.json';
 
 export default function Loading() {
+  const options = {
+    animationData: loadingAnimation,
+    loop: true,
+    autoplay: true,
+  };
+
+  const { View } = useLottie(options);
+
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-white dark:bg-gray-900 z-50">
-      <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-success dark:border-success"></div>
+      <div className="h-32 w-32">{View}</div>
     </div>
   );
 }
